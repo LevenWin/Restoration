9d7e8ea1d27f8b92d57c440a4d2b8915c21f09a1
leven <wen@borderxlab.com>
2017-11-16 17:15:50 +0800

    Initial Commit

---
+++ Restoration/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  Restoration
+//
+//  Created by leven on 2017/11/16.
+//  Copyright © 2017年 leven. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
---
+++ Restoration/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Restoration
+//
+//  Created by leven on 2017/11/16.
+//  Copyright © 2017年 leven. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
